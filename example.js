3930b0ef77a3fcc0464dc993c7b8a3a38e6bf221
yolles <yolles@adobe.com>
2016-08-03 16:58:43 -0700

    Add predefined tonic example for display on npmjs.com

---
+++ example.js
@@ -0,0 +1,17 @@
+var slURI = require("sluri");
+var sluri = new slURI('http://user:pass@www.nateyolles.com/us/en/page.foo.bar.html/biz/baz?a=alpha&b=bravo#charlie');
+
+console.log(sluri.hostname);
+
+console.log(sluri.resourcePath);
+console.log(sluri.extension);
+
+console.log(sluri.selectorString);
+console.log(sluri.selectors.has('foo'));
+sluri.selectors.append('biz');
+sluri.selectors.delete('bar');
+console.log(sluri.selectorString);
+
+console.log(sluri.searchParams.get('a'));
+sluri.searchParams.append('d', 'delta');
+console.log(sluri.search);
